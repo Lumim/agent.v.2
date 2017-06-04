4f44bc833708c674a426fe4a33d03f1071e26018
Mohammad Samiul Islam <forthright48@gmail.com>
2017-06-04 10:49:26 +0600

    Landing page now redirects if logged in

--- controllers/landingPage/index.js
+++ controllers/landingPage/index.js
@@ -1,75 +1,89 @@
-const express = require("express");
-const User = require('mongoose').model('User'); //get
+const express = require('express');
+const User = require('mongoose').model('User'); // get
 const bcrypt = require('bcryptjs');
 const router = express.Router();
 
-router.get('/', function(req, res){
-	res.render("index");
+router.get('/', function(req, res) {
+  if (req.session && req.session.login) { // Already logged in
+    const { username, status } = req.session;
+    if (status === 'faculty') {
+      return res.redirect('/faculty/' + username);
+    } else {
+      return res.send('Not impelemented yet');
+    }
+  } else {
+    return res.render('index');
+  }
 });
 
-router.post('/login', function(req, res){
-	const username = req.body.username;
-	const password = req.body.password;
+router.post('/login', function(req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
 
-	User.findOne({
-		username
-	})
-	.exec(function(err, user){
-		if(err) return res.send('some error occured');
-		if(!user) {
-			//req.flash('error', 'Username not found');
-			return res.send('Wrong password or username');
-		}
-		else{
-			bcrypt.compare(password, user.password, function(err, result){
-				if(err) return res.send('some error occured');
-				if(result == true)
-				{
-					req.session.login = true;
-			        req.session.email = user.email;
-			        req.session.name = user.name;
-                    if(user.status == 'faculty')
-			        	return res.redirect('/faculty/'+username);
-				}
-				else return res.send('Wrong password or username');
-			});
-		}
-	});
-})
+  User
+    .findOne({
+      username,
+    })
+    .exec(function(err, user) {
+      if (err) return res.send('some error occured');
+      if (!user) {
+        // req.flash('error', 'Username not found');
+        return res.send('Wrong password or username');
+      } else {
+        bcrypt.compare(password, user.password, function(err, result) {
+          if (err) return res.send('some error occured');
+          if (result === true) {
+            req.session.login = true;
+            req.session.email = user.email;
+            req.session.name = user.name;
+            req.session.username = username;
+            req.session.status = user.status;
+            if (user.status === 'faculty') {
+              return res.redirect('/faculty/' + username);
+            } else {
+              return res.send('Not impelemented yet');
+            }
+          } else {
+            return res.send('Wrong password or username');
+          }
+        });
+      }
+    });
+});
 
-router.post('/signup', function(req, res ){
-	const name = req.body.name;
-	const email = req.body.email;
-	const username = req.body.username;
-	const school = req.body.school;
-	const country = req.body.country;
-	const password = req.body.password;
-	const status = req.body.portal;
+router.post('/signup', function(req, res) {
+  const name = req.body.name;
+  const email = req.body.email;
+  const username = req.body.username;
+  const school = req.body.school;
+  const country = req.body.country;
+  const password = req.body.password;
+  const status = req.body.portal;
 
-    //Autogen salt and hash
-	bcrypt.hash(password, require('../../secret.js').round, function(err, hash){
-		if (err) return res.send('some error occured');
-		else{
-			const user = new User({
-				name,
-				email,
-				username,
-				school,
-				country,
-				password: hash,
-				status
-			});
+  //Autogen salt and hash
+  bcrypt.hash(password, require('../../secret.js').round, function(err, hash) {
+    if (err) return res.send('some error occured');
+    else {
+      const user = new User({
+        name,
+        email,
+        username,
+        school,
+        country,
+        password: hash,
+        status
+      });
 
-			user.save(function(err){
-				if (err) return res.send('some error occured');
-				return res.send('ok');
-			});
-		}
-	});
+      user.save(function(err) {
+        if (err) return res.send('some error occured');
+        return res.send('ok');
+      });
+    }
+  });
 })
 
 module.exports = {
-	addRouter(app){
-		app.use('/', router);
-	}
+  addRouter(app) {
+    app.use('/', router);
+  }
 }
