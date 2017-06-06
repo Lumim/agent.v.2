f6723e40321a8a91e294e75023b10de63fdb7d7f
nuronial_block <ahnaf93@gmail.com>
2017-06-06 15:57:41 +0600

    Beautify pre-registration

--- controllers/preRegistration/preRegistration.js
+++ controllers/preRegistration/preRegistration.js
@@ -5,18 +5,10 @@ const PreRegistration = mongoose.model("PreRegistration");
 
 router.get('/preregistrations', (req, res) => {
   PreRegistration.find({})
-  // , 'preregistrations', (err, courses) => {
-  //   if(err || courses.length === 0) return res.send("404 NOT FOUND");
-  //   else{
-  //     console.log(courses);
-  //     return res.render("preRegistration", {courses: courses});
-  //   }
-  // })
   .populate('preregistrations')
   .exec((err, courses) => {
     if(err || courses === null) return res.send("404 NOT FOUND");
     else{
-      console.log(courses);
       return res.render("preRegistration", {courses: courses});
     }
   })
