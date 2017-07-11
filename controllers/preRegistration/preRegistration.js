5682e0f3593a219272d9074ce7e4978aa2046a29
nuronial_block <ahnaf93@gmail.com>
2017-07-12 05:21:31 +0600

    Have a nice day!

--- controllers/preRegistration/preRegistration.js
+++ controllers/preRegistration/preRegistration.js
@@ -4,6 +4,9 @@ const router = express.Router();
 const PreRegistration = mongoose.model("PreRegistration");
 
 router.get('/preregistrations', (req, res) => {
+	if (!(req.session && req.session.login)) {
+		return res.send("404 NOT FOUND");
+	}
   PreRegistration.find({})
   .populate('preregistrations')
   .exec((err, courses) => {
@@ -15,6 +18,9 @@ router.get('/preregistrations', (req, res) => {
 })
 
 router.post('/preregistrations/:id', (req, res) => {
+	if (!(req.session && req.session.login)) {
+		return res.send("404 NOT FOUND");
+	}
 	const ID = req.params.id
 	PreRegistration.findOneAndUpdate({"courseID": ID}, {$inc: {"seats": 1}})
 	.exec((err, course) => {
